50ee6aec45490f9f1826c972a753d57d4c67eb97
Viktor Ens <v.ens86@yahoo.de>
2024-10-31 08:30:49 +0100

    add test, navbar styling

--- src/app/components/main/main.component.spec.ts
+++ src/app/components/main/main.component.spec.ts
@@ -1,23 +1,72 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { MainComponent } from './main.component';
+import { MainComponent } from "./main.component";
+import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { FormsModule } from "@angular/forms";
+import { of, throwError } from "rxjs";
+import { UserService } from "../../services/user.service";
 
-describe('MainComponent', () => {
+describe("MainComponent", () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
+  let userServiceMock: any;
 
   beforeEach(async () => {
+    userServiceMock = {
+      getUser: jasmine.createSpy("getUser").and.returnValue(
+        of({
+          login: "testuser",
+          name: "Test User",
+          avatar_url: "https://example.com/avatar.png",
+          created_at: "2020-01-01T00:00:00Z",
+          url: "https://github.com/testuser",
+        })
+      ),
+    };
     await TestBed.configureTestingModule({
-      imports: [MainComponent]
-    })
-    .compileComponents();
+      imports: [MainComponent, FormsModule],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: UserService,
+          useValue: userServiceMock,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should fetch user", () => {
+    //GIVEN
+    const testUsername = "testuser";
+    //WHEN
+    component.fetchUser(testUsername);
+    //THEN
+    expect(userServiceMock.getUser).toHaveBeenCalledWith(testUsername);
+    expect(component.user).toEqual({
+      login: "testuser",
+      name: "Test User",
+      avatar_url: "https://example.com/avatar.png",
+      created_at: "2020-01-01T00:00:00Z",
+      url: "https://github.com/testuser",
+    });
+  });
+
+  it("should handle error when fetching user", () => {
+    userServiceMock.getUser.and.returnValue(
+      throwError((err: string | undefined) => new Error(err))
+    );
+    component.fetchUser("invaliduser");
+
+    expect(component.errorMessage).toBe("Somthing went wrong, Error");
   });
 });
--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,16 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
 
-import { UserService } from './user.service';
+import { UserService } from "./user.service";
+import { provideHttpClient } from "@angular/common/http";
 
-describe('UserService', () => {
+describe("UserService", () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(UserService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
