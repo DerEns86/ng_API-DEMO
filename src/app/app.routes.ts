66e7775121bd422509aff51fdce072424bdc7d16
Viktor Ens <v.ens86@yahoo.de>
2024-10-31 12:11:54 +0100

    add recipe site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
 
-export const routes: Routes = [{ path: '', component: MainComponent }];
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'recipe', component: RecipeComponent },
+];
